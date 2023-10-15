96db4f2332d2b59082134e0f2c299a61246770a4
Gogo200202 <gosheto420@gmail.com>
2023-10-15 14:52:33 +0300

    AbsolutePath fix

--- src/com/database/DataBase.java
+++ src/com/database/DataBase.java
@@ -1,21 +1,11 @@
 package com.database;
-
-import com.database.Interface.AplicationOpetartion;
 import com.database.Interface.DataBaseOperation;
-import com.database.Interface.LoginAndRegisterOperations;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
+
 
 
 
